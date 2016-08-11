f071bdfde7d4c77853426155a46b79a7297e8cfc
Rinnegatamante <rinnegatamante@gmail.com>
2016-08-12 01:31:35 +0200

    Added rumble and multiple DS support. (PSTV only)
    Added rumble and multiple DS support. (PSTV only)

--- source/luaControls.cpp
+++ source/luaControls.cpp
@@ -38,12 +38,16 @@
 #define VariableRegister(lua, value) do { lua_pushinteger(lua, value); lua_setglobal (lua, stringify(value)); } while(0)
 #define lerp(value, from_max, to_max) ((((value*10) * (to_max*10))/(from_max*10))/10)
 
+SceCtrlActuator pad1, pad2;
+
 static int lua_readC(lua_State *L)
 {
     int argc = lua_gettop(L);
-    if (argc != 0) return luaL_error(L, "wrong number of arguments.");
+    if (argc != 0 && argc != 1) return luaL_error(L, "wrong number of arguments.");
+	int port = 0;
+	if (argc == 1) port = luaL_checkinteger(L, 1); 
 	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(0, &pad, 1);
+	sceCtrlPeekBufferPositive(port, &pad, 1);
 	lua_pushnumber(L, pad.buttons);
 	return 1;
 }
@@ -51,9 +55,11 @@ static int lua_readC(lua_State *L)
 static int lua_readleft(lua_State *L)
 {
     int argc = lua_gettop(L);
-    if (argc != 0) return luaL_error(L, "wrong number of arguments.");
+    if (argc != 0 && argc != 1) return luaL_error(L, "wrong number of arguments.");
+	int port = 0;
+	if (argc == 1) port = luaL_checkinteger(L, 1); 
 	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(0, &pad, 1);
+	sceCtrlPeekBufferPositive(port, &pad, 1);
 	lua_pushnumber(L, pad.lx);
 	lua_pushnumber(L, pad.ly);
 	return 2;
@@ -62,9 +68,11 @@ static int lua_readleft(lua_State *L)
 static int lua_readright(lua_State *L)
 {
     int argc = lua_gettop(L);
-    if (argc != 0) return luaL_error(L, "wrong number of arguments.");
+    if (argc != 0 && argc != 1) return luaL_error(L, "wrong number of arguments.");
+	int port = 0;
+	if (argc == 1) port = luaL_checkinteger(L, 1); 
 	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(0, &pad, 1);
+	sceCtrlPeekBufferPositive(port, &pad, 1);
 	lua_pushnumber(L, pad.rx);
 	lua_pushnumber(L, pad.ry);
 	return 2;
@@ -85,10 +93,34 @@ static int lua_touchpad(lua_State *L)
 	SceTouchData touch;
 	sceTouchPeek(SCE_TOUCH_PORT_FRONT, &touch, 1);
 	for (SceUInt32 i=0;i<touch.reportNum;i++){
-		lua_pushnumber(L, lerp(touch.report[i].x, 1919, 960));
-		lua_pushnumber(L, lerp(touch.report[i].y, 1087, 544));
+		lua_pushnumber(L, lerp(touch.report[i].x, 1920, 960));
+		lua_pushnumber(L, lerp(touch.report[i].y, 1088, 544));
 	}
 	return touch.reportNum<<1;
+}
+
+static int lua_rumble(lua_State *L)
+{
+	if (lua_gettop(L) != 2) return luaL_error(L, "wrong number of arguments.");
+	int port = luaL_checkinteger(L, 1);
+	int intensity = luaL_checkinteger(L, 2);
+	switch (port){
+		case 0:
+		case 1:
+			pad1.enable = intensity;
+			pad1.unk = 0;
+			sceCtrlSetActuator(1, &pad1);
+			break;
+		case 2:
+			pad2.enable = intensity;
+			pad2.unk = 0;
+			sceCtrlSetActuator(2, &pad2);
+			break;
+		default:
+			return luaL_error(L, "wrong port number.");
+			break;
+	}	
+	return 0;
 }
 
 static int lua_touchpad2(lua_State *L)
@@ -97,8 +129,8 @@ static int lua_touchpad2(lua_State *L)
 	SceTouchData touch;
 	sceTouchPeek(SCE_TOUCH_PORT_BACK, &touch, 1);
 	for (SceUInt32 i=0;i<touch.reportNum;i++){
-		lua_pushnumber(L, lerp(touch.report[i].x, 1919, 960));
-		lua_pushnumber(L, lerp(touch.report[i].y, 1087, 544));
+		lua_pushnumber(L, lerp(touch.report[i].x, 1920, 960));
+		lua_pushnumber(L, lerp(touch.report[i].y, 1088, 544));
 	}
 	return touch.reportNum<<1;
 }
@@ -108,6 +140,7 @@ static const luaL_Reg Controls_functions[] = {
   {"read",								lua_readC},	
   {"readLeftAnalog",					lua_readleft},	  
   {"readRightAnalog",					lua_readright},	
+  {"rumble",							lua_rumble},	
   {"check",								lua_check},	
   {"readTouch",							lua_touchpad},	
   {"readRetroTouch",					lua_touchpad2},	
@@ -118,6 +151,10 @@ void luaControls_init(lua_State *L) {
 	lua_newtable(L);
 	luaL_setfuncs(L, Controls_functions, 0);
 	lua_setglobal(L, "Controls");
+	uint8_t FIRST_CTRL = 0;
+	uint8_t SECOND_CTRL = 2;
+	VariableRegister(L,FIRST_CTRL);
+	VariableRegister(L,SECOND_CTRL);
 	VariableRegister(L,SCE_CTRL_UP);
 	VariableRegister(L,SCE_CTRL_DOWN);
 	VariableRegister(L,SCE_CTRL_LEFT);
