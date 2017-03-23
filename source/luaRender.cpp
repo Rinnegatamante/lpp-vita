17ff53de8489b32609a31b1a37f6890aac3816a4
Rinnegatamante <rinnegatamante@gmail.com>
2017-03-23 17:42:19 +0100

    Added obj loader.
    Added obj loader.

--- source/luaRender.cpp
+++ source/luaRender.cpp
@@ -53,6 +53,11 @@ struct vertexList{ // We'll use hardcoded GL_TRIANGLES for lpp-3ds compatibility
 	vertexList* next;
 };
 
+struct rawVertexList{
+	vertex* vert;
+	rawVertexList* next;
+};
+
 struct Image {
 	unsigned long sizeX;
 	unsigned long sizeY;
@@ -226,6 +231,275 @@ static int lua_loadmodel(lua_State *L){
 	return 1;
 }
 
+static int lua_loadobj(lua_State *L){
+	int argc = lua_gettop(L);
+    #ifndef SKIP_ERROR_HANDLING
+		if (argc != 2) return luaL_error(L, "wrong number of arguments");
+	#endif
+	const char *file_tbo = luaL_checkstring(L, 1); //Model filename
+	const char* text = luaL_checkstring(L, 2); // Texture filename
+	
+	// Loading texture
+	SceUID file = sceIoOpen(text, SCE_O_RDONLY, 0777);
+	uint16_t magic;
+	sceIoRead(file, &magic, 2);
+	sceIoClose(file);
+	Image* result;
+	if (magic == 0x4D42) result = BitmapLoad(text);
+	//else if (magic == 0xD8FF) result = vita2d_load_JPEG_file(text);
+	//else if (magic == 0x5089) result = vita2d_load_PNG_file(text);
+	else return luaL_error(L, "Error loading image.");
+	#ifndef SKIP_ERROR_HANDLING
+	if (result == NULL) return luaL_error(L, "Error loading image.");
+	#endif
+	
+	// Opening model file and loading it on RAM
+	file = sceIoOpen(file_tbo, SCE_O_RDONLY, 0777);
+	uint32_t size = sceIoLseek(file, 0, SEEK_END);
+	sceIoLseek(file, 0, SEEK_SET);
+	char* content = (char*)malloc(size+1);
+	sceIoRead(file, content, size);
+	content[size] = 0;
+	
+	// Closing file
+	sceIoClose(file);
+	
+	// Creating temp vertexList
+	rawVertexList* vl = (rawVertexList*)malloc(sizeof(rawVertexList));
+	rawVertexList* init = vl;
+	
+	// Init variables
+	char* str = content;
+	char* ptr = strstr(str,"v ");
+	int idx;
+	char float_val[16];
+	char* init_val;
+	char magics[3][3] = {"v ","vt","vn"};
+	int magics_idx = 0;
+	vertex* res;
+	int v_idx = 0;
+	bool skip = false;
+	char* end_vert;
+	char* end_val;
+	float* vert_args;
+	rawVertexList* old_vl;
+	
+	// Vertices extraction
+	for(;;){
+		
+		// Check if a magic change is needed
+		while (ptr == NULL){
+			if (magics_idx < 2){
+				res = init->vert;
+				vl = init;
+				magics_idx++;
+				ptr = strstr(str,magics[magics_idx]);
+			}else{
+				skip = true;
+				break;
+			}
+		}
+		if (skip) break;
+		
+		// Extract vertex
+		if (magics_idx == 0) idx = 0;
+		else if (magics_idx == 1) idx = 3;
+		else idx = 5;
+		if (magics_idx == 0) init_val = ptr + 2;
+		else init_val = ptr + 3;
+		while (init_val[0] == ' ') init_val++;
+		end_vert = strstr(init_val,"\n");
+		if (magics_idx == 0) res = (vertex*)malloc(sizeof(vertex));
+		end_val = strstr(init_val," ");
+		vert_args = (float*)res; // Hacky way to iterate in vertex struct		
+		while (init_val < end_vert){
+			if (end_val > end_vert) end_val = end_vert;
+			strncpy(float_val, init_val, end_val - init_val);
+			float_val[end_val - init_val] = 0;
+			vert_args[idx] = atof(float_val);
+			idx++;
+			init_val = end_val + 1;
+			while (init_val[0] == ' ') init_val++;
+			end_val = strstr(init_val," ");
+		}
+		
+		// Update rawVertexList struct
+		if (magics_idx == 0){
+			vl->vert = res;
+			vl->next = (rawVertexList*)malloc(sizeof(rawVertexList));
+		}
+		old_vl = vl;
+		vl = vl->next;
+		if (magics_idx == 0){
+			vl->vert = NULL;
+			vl->next = NULL;
+		}else{
+			if (vl == NULL){
+				old_vl->next = (rawVertexList*)malloc(sizeof(rawVertexList));
+				vl = old_vl->next;
+				vl->vert = (vertex*)malloc(sizeof(vertex));
+				vl->next = NULL;
+			}else if(vl->vert == NULL) vl->vert = (vertex*)malloc(sizeof(vertex));
+			res = vl->vert;
+		}
+		
+		// Searching for next vertex
+		str = ptr + 1;
+		ptr = strstr(str,magics[magics_idx]);
+		
+	}
+
+	// Creating real RAW vertexList
+	ptr = strstr(str, "f ");
+	rawVertexList* faces = (rawVertexList*)malloc(sizeof(rawVertexList));
+	rawVertexList* initFaces = faces;
+	faces->vert = NULL;
+	faces->next = NULL;
+	int len = 0;
+	char val[8];
+	int f_idx;
+	char* ptr2;
+	int t_idx;
+	rawVertexList* tmp;
+	
+	// Faces extraction
+	while (ptr != NULL){
+		
+		// Skipping padding
+		ptr+=2;		
+		
+		// Extracting face info
+		f_idx = 0;
+		while (f_idx < 3){
+		
+			// Allocating new vertex
+			faces->vert = (vertex*)malloc(sizeof(vertex));
+		
+			// Extracting x,y,z
+			ptr2 = strstr(ptr,"/");
+			strncpy(val,ptr,ptr2-ptr);
+			val[ptr2-ptr] = 0;
+			v_idx = atoi(val);
+			t_idx = 1;
+			tmp = init;
+			while (t_idx < v_idx){
+				tmp = tmp->next;
+				t_idx++;
+			}
+			faces->vert->x = tmp->vert->x;
+			faces->vert->y = tmp->vert->y;
+			faces->vert->z = tmp->vert->z;
+			
+			// Extracting texture info
+			ptr = ptr2+1;
+			ptr2 = strstr(ptr,"/");
+			if (ptr2 != ptr){
+				strncpy(val,ptr,ptr2-ptr);
+				val[ptr2-ptr] = 0;
+				v_idx = atoi(val);
+				t_idx = 1;
+				tmp = init;
+				while (t_idx < v_idx){
+					tmp = tmp->next;
+					t_idx++;
+				}
+				faces->vert->t1 = tmp->vert->t1;
+				faces->vert->t2 = tmp->vert->t2;
+			}else{
+				faces->vert->t1 = 0.0f;
+				faces->vert->t2 = 0.0f;
+			}
+			
+			// Extracting normals info
+			ptr = ptr2+1;
+			if (f_idx < 2) ptr2 = strstr(ptr," ");
+			else{
+				ptr2 = strstr(ptr,"\n");
+				if (ptr2 == NULL) ptr2 = content + size;
+			}
+			strncpy(val,ptr,ptr2-ptr);
+			val[ptr2-ptr] = 0;
+			v_idx = atoi(val);
+			t_idx = 1;
+			tmp = init;
+			while (t_idx < v_idx){
+				tmp = tmp->next;
+				t_idx++;
+			}
+			faces->vert->n1 = tmp->vert->n1;
+			faces->vert->n2 = tmp->vert->n2;
+			faces->vert->n3 = tmp->vert->n3;
+
+			// Setting values for next vertex
+			ptr = ptr2;
+			faces->next = (rawVertexList*)malloc(sizeof(rawVertexList));
+			faces = faces->next;
+			faces->next = NULL;
+			faces->vert = NULL;
+			len++;
+			f_idx++;
+		}
+		
+		ptr = strstr(ptr,"f ");
+		
+	}
+	
+	// Freeing temp vertexList and allocated file
+	free(content);
+	rawVertexList* tmp_init;
+	while (init != NULL){
+		tmp_init = init;
+		free(init->vert);
+		init = init->next;
+		free(tmp_init);
+	}
+	
+	// Create the model struct and populating vertex list
+	if (len % 3 != 0) return luaL_error(L, "invalid model file.");
+	model* res_m = (model*)malloc(sizeof(model));
+	res_m->magic = 0xC0C0C0C0;
+	vertexList* vlist = (vertexList*)malloc(sizeof(vertexList));
+	res_m->v = vlist;
+	vlist->next = NULL;
+	bool first = true;
+	for(int i = 0; i < len; i+=3) {
+		if (first) first = false;
+		else{
+			vlist->next = (vertexList*)malloc(sizeof(vertexList));
+			vlist = vlist->next;
+			vlist->next = NULL;
+		}
+		tmp_init = initFaces;
+		memcpy(&vlist->v1,initFaces->vert,sizeof(vertex));
+		initFaces = initFaces->next;
+		free(tmp_init->vert);
+		free(tmp_init);
+		tmp_init = initFaces;
+		memcpy(&vlist->v2,initFaces->vert,sizeof(vertex));
+		initFaces = initFaces->next;
+		free(tmp_init->vert);
+		free(tmp_init);
+		tmp_init = initFaces;
+		memcpy(&vlist->v3,initFaces->vert,sizeof(vertex));
+		initFaces = initFaces->next;
+		free(tmp_init->vert);
+		free(tmp_init);
+	}
+	
+	// Setting texture
+	glGenTextures(1, &res_m->glTexture[0]);
+	glBindTexture(GL_TEXTURE_2D, res_m->glTexture[0]);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, result->sizeX, result->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, result->data);
+	glEnable(GL_TEXTURE_2D);
+	res_m->texture = result;
+	
+	// Create a model object and push it into Lua stack
+	lua_pushinteger(L, (uint32_t)res_m);
+	return 1;
+}
+
 static int lua_unloadmodel(lua_State *L){
 	int argc = lua_gettop(L);
     #ifndef SKIP_ERROR_HANDLING
@@ -299,6 +573,7 @@ static const luaL_Reg Render_functions[] = {
 	{"createVertex",					lua_newVertex},
 	{"destroyVertex",					lua_delVertex},
 	{"loadModel",						lua_loadmodel},
+	{"loadObject",						lua_loadobj},
 	{"unloadModel",						lua_unloadmodel},
 	{"drawModel",						lua_drawmodel},
 	{0, 0}
