502da5a3551a30c89674473909427b8673c6f37a
Rinnegatamante <rinnegatamante@gmail.com>
2017-07-21 14:41:03 +0200

    Fix identation and cleanup.
    Fix identation and cleanup.

--- samples/3D Cube/index.lua
+++ samples/3D Cube/index.lua
@@ -1,65 +1,65 @@
 -- Create a cube
 model = {
  
-    -- First face (PZ)
-    -- First triangle
-    Render.createVertex(-0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0),
-    Render.createVertex(0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 1.0),
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 1.0),
-    -- Second triangle
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 1.0),
-    Render.createVertex(-0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0),
-    Render.createVertex(-0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0),
+	-- First face (PZ)
+	-- First triangle
+	Render.createVertex(-0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0),
+	Render.createVertex(0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 1.0),
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 1.0),
+	-- Second triangle
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 0.0, 1.0),
+	Render.createVertex(-0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 1.0),
+	Render.createVertex(-0.5, -0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0),
  
-    -- Second face (MZ)
-    -- First triangle
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, -1.0),
-    Render.createVertex(-0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 0.0, -1.0),
-    Render.createVertex(0.5, 0.5, -0.5, 1.0, 1.0, 0.0, 0.0, -1.0),
-    -- Second triangle
-    Render.createVertex(0.5, 0.5, -0.5, 1.0, 1.0, 0.0, 0.0, -1.0),
-    Render.createVertex(0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, -1.0),
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, -1.0),
+	-- Second face (MZ)
+	-- First triangle
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, -1.0),
+	Render.createVertex(-0.5, 0.5, -0.5, 1.0, 0.0, 0.0, 0.0, -1.0),
+	Render.createVertex(0.5, 0.5, -0.5, 1.0, 1.0, 0.0, 0.0, -1.0),
+	-- Second triangle
+	Render.createVertex(0.5, 0.5, -0.5, 1.0, 1.0, 0.0, 0.0, -1.0),
+	Render.createVertex(0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, -1.0),
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, 0.0, -1.0),
  
-    -- Third face (PX)
-    -- First triangle
-    Render.createVertex(0.5, -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0),
-    Render.createVertex(0.5, 0.5, -0.5, 1.0, 0.0, 1.0, 0.0, 0.0),
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 0.0),
-    -- Second triangle
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 0.0),
-    Render.createVertex(0.5, -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0),
-    Render.createVertex(0.5, -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0),
+	-- Third face (PX)
+	-- First triangle
+	Render.createVertex(0.5, -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0),
+	Render.createVertex(0.5, 0.5, -0.5, 1.0, 0.0, 1.0, 0.0, 0.0),
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 0.0),
+	-- Second triangle
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 0.0),
+	Render.createVertex(0.5, -0.5, 0.5, 0.0, 1.0, 1.0, 0.0, 0.0),
+	Render.createVertex(0.5, -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0),
  
-    -- Fourth face (MX)
-    -- First triangle
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0),
-    Render.createVertex(-0.5, -0.5, 0.5, 1.0, 0.0, -1.0, 0.0, 0.0),
-    Render.createVertex(-0.5, 0.5, 0.5, 1.0, 1.0, -1.0, 0.0, 0.0),
-    -- Second triangle
-    Render.createVertex(-0.5, 0.5, 0.5, 1.0, 1.0, -1.0, 0.0, 0.0),
-    Render.createVertex(-0.5, 0.5, -0.5, 0.0, 1.0, -1.0, 0.0, 0.0),
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0),
+	-- Fourth face (MX)
+	-- First triangle
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0),
+	Render.createVertex(-0.5, -0.5, 0.5, 1.0, 0.0, -1.0, 0.0, 0.0),
+	Render.createVertex(-0.5, 0.5, 0.5, 1.0, 1.0, -1.0, 0.0, 0.0),
+	-- Second triangle
+	Render.createVertex(-0.5, 0.5, 0.5, 1.0, 1.0, -1.0, 0.0, 0.0),
+	Render.createVertex(-0.5, 0.5, -0.5, 0.0, 1.0, -1.0, 0.0, 0.0),
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0),
  
-    -- Fifth face (PY)
-    -- First triangle
-    Render.createVertex(-0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0),
-    Render.createVertex(-0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0),
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0),
-    -- Second triangle
-    Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0),
-    Render.createVertex(0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0),
-    Render.createVertex(-0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0),
+	-- Fifth face (PY)
+	-- First triangle
+	Render.createVertex(-0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0),
+	Render.createVertex(-0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0),
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0),
+	-- Second triangle
+	Render.createVertex(0.5, 0.5, 0.5, 1.0, 1.0, 0.0, 1.0, 0.0),
+	Render.createVertex(0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 0.0),
+	Render.createVertex(-0.5, 0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0),
  
-    -- Sixth face (MY)
-    -- First triangle
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, -1.0, 0.0),
-    Render.createVertex(0.5, -0.5, -0.5, 1.0, 0.0, 0.0, -1.0, 0.0),
-    Render.createVertex(0.5, -0.5, 0.5, 1.0, 1.0, 0.0, -1.0, 0.0),
-    -- Second triangle
-    Render.createVertex(0.5, -0.5, 0.5, 1.0, 1.0, 0.0, -1.0, 0.0),
-    Render.createVertex(-0.5, -0.5, 0.5, 0.0, 1.0, 0.0, -1.0, 0.0),
-    Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, -1.0, 0.0)
+	-- Sixth face (MY)
+	-- First triangle
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, -1.0, 0.0),
+	Render.createVertex(0.5, -0.5, -0.5, 1.0, 0.0, 0.0, -1.0, 0.0),
+	Render.createVertex(0.5, -0.5, 0.5, 1.0, 1.0, 0.0, -1.0, 0.0),
+	-- Second triangle
+	Render.createVertex(0.5, -0.5, 0.5, 1.0, 1.0, 0.0, -1.0, 0.0),
+	Render.createVertex(-0.5, -0.5, 0.5, 0.0, 1.0, 0.0, -1.0, 0.0),
+	Render.createVertex(-0.5, -0.5, -0.5, 0.0, 0.0, 0.0, -1.0, 0.0)
    
 }
 
@@ -79,29 +79,29 @@ translY = 0.0034
 while true do
  
 	-- Rotate the model
-    angleX = angleX + 0.2
-    angleY = angleY + 0.2
+	angleX = angleX + 0.2
+	angleY = angleY + 0.2
 	
 	-- Move the model
-    x = x + translX
-    y = y + translY
+	x = x + translX
+	y = y + translY
 	
 	-- Blend the model with info on screen
 	Graphics.initBlend()
 	Screen.clear()
-    Render.drawModel(mod1, math.sin(x), math.cos(y), z, angleX, angleY)
+	Render.drawModel(mod1, math.sin(x), math.cos(y), z, angleX, angleY)
 	Graphics.debugPrint(3, 3, "Lua Player Plus Vita - Render Module Sample - Cube Test", Color.new(255,255,255))
 	Graphics.debugPrint(3, 23, "Press TRIANGLE to return to the main menu.", Color.new(255,255,255))
 	Graphics.termBlend()
 	Screen.flip()
 	
-   -- Exit sample
-    if Controls.check(Controls.read(), SCE_CTRL_TRIANGLE) then
-        Render.unloadModel(mod1)
+	-- Exit sample
+	if Controls.check(Controls.read(), SCE_CTRL_TRIANGLE) then
+		Render.unloadModel(mod1)
 		for i, vertex in pairs(model) do
 			Render.destroyVertex(vertex)
 		end
-        break
-    end
+		break
+	end
    
 end
--- samples/3D Model/index.lua
+++ samples/3D Model/index.lua
@@ -20,26 +20,26 @@ translY = 0.0034
 while true do
  
 	-- Rotate the model
-    angleX = angleX + 0.2
-    angleY = angleY + 0.2
+	angleX = angleX + 0.2
+	angleY = angleY + 0.2
 	
 	-- Move the model
-    x = x + translX
-    y = y + translY
+	x = x + translX
+	y = y + translY
 	
 	-- Blend the model with info on screen
 	Graphics.initBlend()
 	Screen.clear()
-    Render.drawModel(mod1, math.sin(x), math.cos(y), z, angleX, angleY)
+	Render.drawModel(mod1, math.sin(x), math.cos(y), z, angleX, angleY)
 	Graphics.debugPrint(3, 3, "Lua Player Plus Vita - Render Module Sample - 3D Model Test", Color.new(255,255,255))
 	Graphics.debugPrint(3, 23, "Press TRIANGLE to return to the main menu.", Color.new(255,255,255))
 	Graphics.termBlend()
 	Screen.flip()
 	
-   -- Exit sample
-    if Controls.check(Controls.read(), SCE_CTRL_TRIANGLE) then
-        Render.unloadModel(mod1)
-        break
-    end
+	-- Exit sample
+	if Controls.check(Controls.read(), SCE_CTRL_TRIANGLE) then
+		Render.unloadModel(mod1)
+		break
+	end
    
 end
