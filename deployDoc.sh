03dec5a86f53895b7c9e36818e6dce0a0c45b881
Rinnegatamante <rinnegatamante@gmail.com>
2017-07-26 19:25:14 +0200

    Moving from html to root the doc.
    Moving from html to root the doc.

--- deployDoc.sh
+++ deployDoc.sh
@@ -81,6 +81,10 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 if [ -d "html" ] && [ -f "html/index.html" ]; then
 
     echo 'Uploading documentation to the gh-pages branch...'
+	
+	mv html/* .
+	rm -rf html
+	
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
     # GitHub is smart enough to know which files have changed and which files have
