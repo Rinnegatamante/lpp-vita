c5729f72c48c1f831230d1d2f81f29f705b74a9e
Travis CI <travis@travis-ci.org>
2017-09-28 14:27:37 +0000

    Deploy code docs to GitHub Pages Travis build: 98
    Commit: 5c5e66eca4aac9f747f3dc5724927be54253d317

--- group___video.js
+++ group___video.js
@@ -3,6 +3,7 @@ var group___video =
     [ "init", "group___video.html#ga5ef29e5f38ca9ee087bb003e6da038c3", null ],
     [ "term", "group___video.html#gaa70ed8a319fc43c04b096966decc71b6", null ],
     [ "open", "group___video.html#ga1e4e8f340217df9dc3ca32969a12befc", null ],
+    [ "close", "group___video.html#gafa6a2646049cfdd140bd932137847751", null ],
     [ "getOutput", "group___video.html#gae7e2bb6ca761e532893d643109048407", null ],
     [ "pause", "group___video.html#ga83b4d74d47635f008d95137ca8a3f077", null ],
     [ "resume", "group___video.html#gaf13c933cf7cd12507799d57d2cfa9241", null ],
