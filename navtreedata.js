77d517f11f43802e4394fd0baff59d6df110faf5
Travis CI <travis@travis-ci.org>
2019-07-24 22:11:46 +0000

    Deploy code docs to GitHub Pages Travis build: 635
    Commit: 00c869f377a0d86912fc5369075f0189208a487f

---
+++ navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "Lua Player Plus Vita", "index.html", [
+    [ "Lua Player Plus Vita - Documentation", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group___camera.html",
+"group___system.html#ga0dfeddb6754b9bdf9e5db8c568b1d127"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghiklmnopqrstuvw",
+  1: "cdfgkmnrstv",
+  2: "acdefghilnoprstuw",
+  3: "bcdhiklnps",
+  4: "abcdefghilmnopqrstuvw",
+  5: "cdfgkmnrstv",
+  6: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "enums",
+  4: "enumvalues",
+  5: "groups",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Enumerations",
+  4: "Enumerator",
+  5: "Modules",
+  6: "Pages"
+};
+
