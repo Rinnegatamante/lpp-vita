94bba1088122a4dbb12c7b21ae0062e295213716
Rinnegatamante <rinnegatamante@gmail.com>
2025-04-27 13:31:23 +0000

    Deploying to gh-pages from @ Rinnegatamante/lpp-vita@88b3498f6fc113b69db140b206d610bc62e3f12a 🚀

--- search/functions_11.js
+++ search/functions_11.js
@@ -5,7 +5,7 @@ var searchData=
   ['unloadmodel_2',['unloadModel',['../group___render.html#ga8e217951fc915be273a4f898d2247138',1,'Render']]],
   ['unloaduserplugin_3',['unloaduserplugin',['../group___system.html#ga9bdabd2a38b4ad66e1faa53ccbaf183b',1,'System::unloadUserPlugin(int plug_id)'],['../class_system.html#ga9bdabd2a38b4ad66e1faa53ccbaf183b',1,'System::unloadUserPlugin(int plug_id)']]],
   ['unlockhomebutton_4',['unlockHomeButton',['../group___controls.html#gacec557849101bb976fbc2ce145cf0ab4',1,'Controls']]],
-  ['unmountmountpoint_5',['unmountMountpoint',['../group___system.html#ga75877661c3f115df6d5164e0d028969d',1,'System']]],
+  ['unmountmountpoint_5',['unmountmountpoint',['../group___system.html#ga75877661c3f115df6d5164e0d028969d',1,'System::unmountMountpoint(string mnt)'],['../class_system.html#ga75877661c3f115df6d5164e0d028969d',1,'System::unmountMountpoint(string text)']]],
   ['unmountpartition_6',['unmountPartition',['../group___system.html#gaf0eac5050dc874353fe4e06f4834e5d3',1,'System']]],
   ['usetexture_7',['useTexture',['../group___render.html#ga1f5538469280f46cfc62abb9390ce5b0',1,'Render']]]
 ];
